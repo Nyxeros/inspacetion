7393faf43f6960fe32101689e62cec2065e1036e
michaelroth <mroth@highbridgecreative.com>
2013-04-19 14:13:08 -0600

    Click to move, misc fixes and code cleanup

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var namerand = Math.floor(Math.random() * 999);
-var playername = "pilot" + namerand;
+var shipname = "pilot" + namerand;
 
 var socket = io.connect('http://localhost:8080');
 
@@ -12,23 +12,22 @@ socket.on('disconnectmessage', function(data) {
 });
 
 // on player move
-socket.on('playermove', function(positionx, positiony, currentanimation,thisgamename) {
+socket.on('shipmove', function(destinationx, destinationy, currentanimation, thisgamename) {
+	
 	var otherplayer = ig.game.getEntitiesByType(EntityOthership);
 
 	if (otherplayer) {
 		for ( var i in otherplayer) {
 			if (thisgamename == otherplayer[i].gamename) {
-				otherplayer[i].pos.x = positionx;
-				otherplayer[i].pos.y = positiony;
-
-				otherplayer[i].animation = currentanimation;
+				otherplayer[i].destinationx = destinationx;
+				otherplayer[i].destinationy = destinationy;
 			}
 		}
 	}
 });
 
 // check players
-socket.on('netreplayer', function(playerlist) {
+socket.on('syncship', function(playerx,playery,thisgamename) {
 	var netplayers = ig.game.getEntitiesByType(EntityOthership);
 	
 	// check if players exist
@@ -37,9 +36,9 @@ socket.on('netreplayer', function(playerlist) {
 			netplayers[i].kill();
 		}
 	}
-
+	// loop through plarers and re-initiate
 	for ( var i in playerlist) {
-		if (playername != playerlist[i]) {
+		if (shipname != playerlist[i]) {
 			ig.game.spawnEntity(EntityOthership, 160, 260, {
 				gamename : playerlist[i]
 			});
@@ -49,16 +48,83 @@ socket.on('netreplayer', function(playerlist) {
 });
 
 // new player
-socket.on('addplayer', function(playerlist, otherplayername) {
+socket.on('addship', function(playerlist, othershipname) {
 	
 	var player = ig.game.getEntitiesByType(EntityShip)[0];
 	
-	player.messagebox.content = player.messagebox.content + "\n" + otherplayername + ' joined';
+	player.messagebox.content = player.messagebox.content + "\n" + othershipname + ' joined';
 	for ( var i = 0; i < playerlist.length; i++) {
 		if (player.gamename != playerlist[i]) {
 			ig.game.spawnEntity(EntityOthership, 160, 260, { gamename : playerlist[i] });
 			
 		}
 	}
+});
 
+//kill player
+socket.on('killship', function(othershipname) {
+	
+	var netplayers = ig.game.getEntitiesByType(EntityOthership);
+		
+		if(netplayers){
+			for(var i in netplayers){
+				if(othershipname == netplayers[i].gamename){
+					netplayers[i].kill();
+				}
+			}
+		}
+
+});
+
+//netreplayer
+socket.on('netreship', function(playerlist){
+	var netplayers = ig.game.getEntitiesByType(EntityOthership);
+	
+	if(netplayers){
+		for(var i in netplayers){
+			netplayers[i].kill();
+		}
+		
+		for(var i in playerlist){
+			if(shipname != playerlist[i]){
+				ig.game.spawnEntity(EntityOthership, 160, 260, {gamename:playerlist[i]});
+			}
+		}
+	}
+});
+
+/* ----------- lazers ----------- */
+
+// spawn a bullet
+socket.on('spawnclientbullet', function(weapontype, gamename, animangle){
+	
+	var netplayers = ig.game.getEntitiesByType(EntityOthership);
+	
+	if(netplayers){
+		for(var i=0; i<netplayers.length; i++ ){
+			if(netplayers[i].gamename == gamename){
+				ig.game.spawnEntity(
+						EnityNetlazer, 
+						netplayers[i].pos.x + 30, 
+						netplayers[i].pos.y + 30, 
+						{bullettype:weapontype, animangle: animangle} 
+						);
+			}
+		}
+	}
+	
+});
+
+
+/* --------- creatures -------- */
+socket.on('spawncreature', function(spawnx, spawny, creaturename){
+	ig.game.spawnEntity(EntityCreature, spawnx, spawny, {gamename: creaturename});
+});
+
+socket.on('resynccreature',function(){
+	
+});
+
+socket.on('creaturemove', function(){
+	
 });
--- server/app.js
+++ server/app.js
@@ -5,45 +5,101 @@ var app = require('http').createServer(handler),
 app.listen(8080);
 
 // players
-var playerlocation = 0;
-var playerlist = [];
+var playerlocation = 0,
+	playerlist = [],
+	creaturecount = 0,
+	creaturelist = [];
 
+clientids = [];
+
+
+/**
+ * handler
+ * @param req
+ * @param res
+ * @returns void
+ */
 function handler(req, res) {
 	fs.readFile(__dirname + '/index.html', function(err, data) {
 		if (err) {
 			res.writeHead(500);
 			return res.end('Error loading index.html');
 		}
-
 		res.writeHead(200);
 		res.end(data);
 	});
 }
 
+
+/**
+ * creature1
+ * @returns void
+ */
+function creature1(){
+	this.health = 10;
+	this.target = "";
+	this.velx = 0;
+	this.vely = 0;
+	this.positionx = 500;
+	this.positiony = 0;
+	this.name = "";
+}
+
 // socket connection
 io.sockets.on('connection', function(socket) {
-
-	socket.on('recievedata', function(positionx, positiony, currentanimation, gamename) {
-		socket.broadcast.emit('playermove', positionx, positiony, currentanimation, gamename);
-	});
-
-	socket.on('initializeplayer', function(newplayername) {
-		socket.clientname = newplayername;
-		playerlist.push(newplayername);
-		io.sockets.emit('addplayer', playerlist, newplayername);
-
+	
+	// move a player
+	socket.on('moveship', function(destinationx, destinationy, gamename) {
+		io.sockets.emit('shipmove', destinationx, destinationy, gamename);
 	});
 	
+	// resyncplayer
+	socket.on('resyncship', function(playerx, playery, gamename) {
+		io.sockets.emit('syncship', playerx, playery, gamename);
+	});
+	
+	// playerattacking
+	socket.on('shipattacking', function(direction, xadd, yadd,attackangle, gamename) {
+		io.sockets.emit('shipattacked', direction, xadd, yadd, attackangle, gamename);
+	});
+	
+	// initializeplayer
+	socket.on('initializeship', function(newplayername, playerx, playery) {
+		socket.clientname = newplayername;
+		playerlist.push(newplayername);
+		clientids.push(socket.id);
+		
+		io.sockets.emit('addship', playerlist, newplayername, playerx, playery);
+	});
+	
+	// disconnect
 	socket.on('disconnect', function() {
+		io.sockets.emit('killship', socket.clientname);
 		delete playerlist[socket.clientname];
-		for ( var i in playerlist) {
-			if (playerlist[i] == socket.clientname) {
+		delete clientids[socket.id];
+		
+		for(var i in playerlist){
+			if(playerlist[i] == socket.clientname){
 				playerlist.splice(i, 1);
 			}
 		}
-		socket.broadcast.emit('disconnectmessage', socket.clientname);
-		socket.broadcast.emit('netreplayer', playerlist);
-
+		
+		for(var i in clientids){
+			if(clientids[i] == socket.id){
+				clientids.splice(i,1);
+			}
+		}
+		socket.emit('message', socket.clientname);
 	});
-
+	
+	// spawn a bullet
+	socket.on('spawnbullet', function(weapontype, gamename, angle){
+		io.sockets.emit('spawnclientbullet', weapontype, gamename, angle);
+	});
+	
+	// sync creatures
+	socket.on('syncreatures', function(creatures){
+		
+	});
+	
 });
